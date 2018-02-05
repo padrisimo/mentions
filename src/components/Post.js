5d39177d5c9b0a9c6d7733d75f8c1f9e274260be
padrisimo <chamizho@gmail.com>
2018-02-05 23:01:46 +0100

    set text into state

--- src/components/Post.js
+++ src/components/Post.js
@@ -6,48 +6,49 @@ import { fetchUsers } from '../actions';
 
 
 class Post extends Component {
-  state = { text: ''}
-
+  state = { text: "" }
+  
   componentWillMount = () => {
     if (!this.props.isfetched) {
       this.props.fetchUsers();
     }
   }
-
-  observeMentions(text){
-    console.log(text)
-    this.setState({ text })
-  }
-
-  render() {
-    if (!this.props.isfetched) {
-      return <Spinner />
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.text) {
+      console.log(this.state.text)
     }
-
-    return (
-      <Container>
-        <Content style={{ marginLeft: 10, marginRight: 10 }}>
-          <Form>
-            <Item regular style={{ marginBottom: 10, marginTop: 10, borderColor: 'gray', borderWidth: 1 }}>
-              <Input
-                multiline={true}
-                onChange={text => this.observeMentions(text)}
-                style={{ height: 300, borderWidth: 0}}
-                value={this.state.text}
-                placeholder='post some stuff here about gihub users' />
-            </Item>
-            <Button
-              dark
-              block
-              style={{ marginBottom: 10 }}
-              onPress={() => alert('yea')}>
-              <Text>post it!</Text>
-            </Button>
-          </Form>
-        </Content>
-      </Container>
-    )
   }
+  
+
+render() {
+  if (!this.props.isfetched) {
+    return <Spinner />
+  }
+
+  return (
+    <Container>
+      <Content style={{ marginLeft: 10, marginRight: 10 }}>
+        <Form>
+          <Item regular style={{ marginBottom: 10, marginTop: 10, borderColor: 'gray', borderWidth: 1 }}>
+            <Input
+              multiline={true}
+              onChangeText={text => this.setState({text})}
+              style={{ height: 300, borderWidth: 0 }}
+              //value={this.state.text}
+              placeholder='post some stuff here about gihub users' />
+          </Item>
+          <Button
+            dark
+            block
+            style={{ marginBottom: 10 }}
+            onPress={() => alert('yea')}>
+            <Text>post it!</Text>
+          </Button>
+        </Form>
+      </Content>
+    </Container>
+  )
+}
 }
 
 const mapStateToProps = (state) => ({
