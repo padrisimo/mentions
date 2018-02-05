f62386cbbbc4005f586701ac20567952f20c4361
padrisimo <nopuedoparardemolar@outlook.com>
2018-02-05 11:24:14 +0100

    create store

--- src/App.js
+++ src/App.js
@@ -1,52 +1,26 @@
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { StyleProvider } from 'native-base';
+import AppNavigator from './app.navigator';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+import { logger } from 'redux-logger';
+import reducers from './reducers';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import getTheme from '../native-base-theme/components';
+import custom from '../native-base-theme/variables/custom';
+
+const createStoreWithMiddleware = applyMiddleware(logger, reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
 
 export default class App extends Component {
-
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native ou yeaa!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <Provider store={store} >
+        <StyleProvider style={getTheme(custom)}>
+          <AppNavigator />
+        </StyleProvider>
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
