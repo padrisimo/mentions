d196400baeca3afc1d167ab122fb923692ad02fc
padrisimo <nopuedoparardemolar@outlook.com>
2018-02-05 13:45:06 +0100

    basic routing

--- src/app.navigator.js
+++ src/app.navigator.js
@@ -1,7 +1,35 @@
 import React from 'react';
 import { DrawerNavigator } from 'react-navigation';
+import Post from './components/Post';
+import Mentions from './components/Mentions';
 
-const RouteConfig = {};
-const AppNavigator = DrawerNavigator({}, RouteConfig);
+const PostScreen = {
+  screen: Post,
+  navigationOptions: {
+    headerMode: 'screen',
+    headerTitle: 'Post',
+    drawerLabel: 'Post'
+
+  }
+}
+
+const MentionsScreen = {
+  screen: Mentions,
+  navigationOptions: {
+    headerMode: 'screen',
+    headerTitle: 'Mentions',
+    drawerLabel: 'Mentions'
+
+  }
+}
+
+const RouteConfig = {
+  initialRoute: 'PostScreen'
+}
+
+const AppNavigator = DrawerNavigator({
+  Post: PostScreen,
+  Mentions: MentionsScreen
+}, RouteConfig);
 
 export default AppNavigator;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
 
-export default combineReducers({
-    state: {}
+const rootReducer = combineReducers({
+  state: (state = {}) => state
 });
+
+export default rootReducer;
