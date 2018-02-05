b8314d9e051071a2889621bd0589a2a7e5006e5b
padrisimo <chamizho@gmail.com>
2018-02-05 16:45:33 +0100

    adding button

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react'
-import { Container, Header, Content, Input, Item, Label } from 'native-base';
+import { Container, Text, Header, Content, Input, Button, Item, Form, Label } from 'native-base';
 import { NavigationActions } from 'react-navigation';
 
 export default class Post extends Component {
   render() {
     return (
       <Container>
-        <Content>
-          <Item regular>
-            <Input 
-              multiline={true}
-              style={{height: 400, borderColor: 'gray', borderWidth: 1, marginTop: 10}}
-              placeholder='Regular Textbox'/>
-          </Item>
+        <Content style={{ marginLeft: 10, marginRight: 10}}>
+          <Form>
+            <Item regular style={{ marginBottom: 10 }}>
+              <Input
+                multiline={true}
+                style={{ height: 300, borderColor: 'gray', borderWidth: 1, marginTop: 10 }}
+                placeholder='post some stuff here about gihub users' />
+            </Item>
+            <Button
+              dark
+              block
+              style={{ marginBottom: 10 }}
+              onPress={() => alert('yea')}>
+              <Text>Post</Text>
+            </Button>
+          </Form>
         </Content>
       </Container>
     )
